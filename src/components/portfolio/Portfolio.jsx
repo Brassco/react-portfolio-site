d0bb3a021540b363dfc2dbbd23b058da1c076538
Brassco <tonny.brassco@gmail.com>
2022-05-23 17:57:42 +0300

    Created base structure

---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import './portfolio.css';
+
+const Portfolio = props => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+Portfolio.propTypes = {
+
+}
+
+export default Portfolio
